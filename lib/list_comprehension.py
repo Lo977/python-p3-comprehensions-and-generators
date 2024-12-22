fe010940d0f6ecd0eae86d0491ec815bf56c5639
Buddha Lo <lo_977@yahoo.com>
2024-12-21 23:59:23 -0800

    done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-
+num_list = [7,2,4,5,6,6]
 def return_evens(num_list):
     return [num for num  in num_list if num%2==0]
 
 def make_exclamation(sentence_list):
     return [sentence + "!" for sentence in sentence_list]
+
+print(return_evens(num_list))
